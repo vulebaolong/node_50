35d844a675bface26fe7147b1efdaf8b83223980
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-03-02 17:59:21 +0700

    first commit

---
+++ road_map.js
@@ -0,0 +1,87 @@
+const roadMap ={
+   MySQL: {
+      "Buổi 1": "Khải giảng",
+      "Buổi 2": "Basic SQL",
+      "Buổi 3": "Query SQL, Relationship"
+   },
+   ExpressJS: {
+      "Buổi 4": [
+         "- Làm quen với ExpressJS",
+         "- MySQL2, Kết Nối BE với DB",
+         "- Kết nối 3 hệ thống FE, BE, DB",
+         "- CORS",
+      ],
+      "Buổi 5": [
+         "- ORM Sequelize",
+         "- Code First, Database Fist",
+         "- ORM Prisma"
+      ],
+      "Buổi 6": [
+         "- Tìm hiểu mô hình MVC"
+      ],
+      "Buổi 7": [
+         "- Xử lý lỗi"
+      ]
+   },
+   Auth: {
+      "Buổi 8": [
+         "- Xây dựng api đăng nhập, đăng ký",
+         "- Mã hoá password",
+         "- Login FB, Demo Hack FB"
+      ],
+      "Buổi 9": [
+         "- Gửi email",
+         "- Cấu trúc token",
+         "- Access Token (jsonwebtoken)"
+      ],
+      "Buổi 10": [
+         "- Refresh Token (jsonwebtoken)",
+         "- Middleware bảo vệ api",
+      ],
+      "Buổi 11": [
+         "- Phần Quyền",
+         "- Role, Permission"
+      ],
+      "Buổi 12": [
+         "- Viết Unit Test Với Jest",
+         "- Debug trên vscode",
+         "- Swagger Open API"
+      ]
+   },
+   Prisma: {
+      "Buổi 13": [
+         "- Xây dựng api get-info",
+         "- Chức năng đăng nhập với GA (Google Authenticator)"
+      ],
+      "Buổi 14": [
+         "- GraphQL",
+         "- Upload hình ảnh Local / Cloud",
+         "- Ráp API và FE"
+      ],
+      "Buổi 15": [
+         "- Socket IO",
+         "- Xây dựng chức năng chat"
+      ]
+   },
+   NestJS: {
+      "Buổi 16": [
+         "- Cấu trúc module",
+         "- Upload Image",
+         "- Swagger",
+         "- Biến Môi Trường .env"
+      ],
+      "Buổi 17": [
+         "- Xử lý Token với passport, stategy"
+      ]
+   },
+   Docker: {
+      "Buổi 18": [
+         "- Tìm hiểu Docker",
+         "- Tìm hiểu docker-compose"
+      ],
+      "Buổi 19": [
+         "- Deploy hoàn chỉnh lên server",
+         "- CI/CD"
+      ]
+   }
+}
